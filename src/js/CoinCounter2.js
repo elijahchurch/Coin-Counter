aa78b304fd7453bf4309315c43e380b92d53f9f0
Suzanne <SuzanneSchuber@gmail.com>
2023-08-28 16:47:02 -0700

    Defined CoinCounter2 logic. WIP: getting syntax to work Co-authored by: Michael Carroll mcarroll138@gmail.com

--- src/js/CoinCounter2.js
+++ src/js/CoinCounter2.js
@@ -1,3 +1,20 @@
 export default function CoinCounter2 (money)
 {
+    if (isNaN(money) || money <= 0){
+        return money;
+    }
+    const quarters = Math.trunc(money / .25);
+    const dimeChange = money - (quarters * .25);
+    return function dimeChanger(dimeChange){
+        const dimes = Math.trunc(dimeChange / .10);
+        const nickelChange = dimeChange - (dimes * .10);
+        return function nickelChanger(nickelChange){
+            const nickels = Math.trunc(nickelChange / .05);
+            const pennyChange = nickelChange - (nickels * .05);
+            return function pennyChanger(pennyChange){
+                const pennies = pennyChange / .01;
+                return "You have ${quarters} quarters, ${dimes} dimes, ${nickels} nickels, ${pennies} pennies!";
+            }
+        }
+    }
 }
