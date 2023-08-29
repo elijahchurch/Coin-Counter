fbf303694f80a54f1415d6ee45c9c8de8946c310
Jason E. Church <elijahchurch@gmail.com>
2023-08-29 15:22:09 -0700

    FIX: tweaked logic to coinCounter 2 to make it successfully run

--- src/js/CoinCounter2.js
+++ src/js/CoinCounter2.js
@@ -1,19 +1,20 @@
-export default function CoinCounter2 (money)
+export default function coinCounter2 (money)
 {
     if (isNaN(money) || money <= 0){
         return money;
-    }
+    } else {
     const quarters = Math.trunc(money / .25);
     const dimeChange = money - (quarters * .25);
-    return function dimeChanger(dimeChange){
+    return function dimeChanger(){
         const dimes = Math.trunc(dimeChange / .10);
         const nickelChange = dimeChange - (dimes * .10);
-        return function nickelChanger(nickelChange){
+        return function nickelChanger(){
             const nickels = Math.trunc(nickelChange / .05);
             const pennyChange = nickelChange - (nickels * .05);
-            return function pennyChanger(pennyChange){
-                const pennies = pennyChange / .01;
-                return "You have ${quarters} quarters, ${dimes} dimes, ${nickels} nickels, ${pennies} pennies!";
+            return function pennyChanger(){
+                const pennies = Math.round(pennyChange / .01);
+                return `You have ${quarters} quarters, ${dimes} dimes, ${nickels} nickels, ${pennies} pennies!`;
+                }
             }
         }
     }
